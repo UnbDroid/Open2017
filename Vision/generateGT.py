b776caeae6016b0d68bfcac3b00938515354ae87
Bruno Andreghetti Dantas <brunoandreghetti@gmail.com>
2017-06-11 18:09:46 -0300

    Moved main section of code into main() function

--- Vision/generateGT.py
+++ Vision/generateGT.py
@@ -1,15 +1,13 @@
-					#Code to generate Ground Truth images - Cow chalenge
+#Code to generate Ground Truth images - Cow chalenge
 
 import cv2
 import numpy as np
-
 
 
 #_______________________________________________________________________________________________________________________________
 def mouse(e, x, y, flags, param):
 	global key
 	global GT
-	
 
 	if e == cv2.EVENT_LBUTTONDOWN:
 		print ("X = %d  Y = %d"%(x, y))
@@ -17,7 +15,7 @@ def mouse(e, x, y, flags, param):
 			GT[y, x] = [255, 255, 255]			#Points in the cow body are in white
 		else:
 			GT[y, x] = [0, 0, 255]				#Points in the cow legs are in red
-		cv2.imshow("%sGT" %(num), GT)
+		cv2.imshow("%03dGT" % num, GT)
 
 #________________________________________________________________________________________________________________________
 	#Function to isolate the name passed (taking of any .jpg, or address before '/')
@@ -33,42 +31,44 @@ def GetRawName(name):
 
 #________________________________________________________________________________________________________________________
 
-		#Main
-for num in range(input("Primeira foto: "), input("Ultima foto: ") +1):
+def main():
+	for num in range(input("Primeira foto: "), input("Ultima foto: ") +1):
 
-		#Leitura da imagem
-	#imgName = raw_input("Nome da imagem: ")
-	img = cv2.imread("src/%d.jpg" %(num))
-	#name = GetRawName(imgName)
+			#Leitura da imagem
+		#imgName = raw_input("Nome da imagem: ")
+		img = cv2.imread("src/%d.jpg" %(num))
+		#name = GetRawName(imgName)
 
-		#Imagem Ground Truth
-	height = img.shape[0]
-	width = img.shape[1]
-	GT = np.zeros((height, width, 3))
+			#Imagem Ground Truth
+		height = img.shape[0]
+		width = img.shape[1]
+		GT = np.zeros((height, width, 3))
 
-		#Janelas
-	cv2.namedWindow("%dGT" %(num))
-	cv2.imshow("%dGT" %(num), GT)
+			#Janelas
+		cv2.namedWindow("%dGT" %(num))
+		cv2.imshow("%dGT" %(num), GT)
 
-	cv2.namedWindow("%d"%(num), cv2.WINDOW_NORMAL)
-	cv2.imshow("%d"%(num), img)
-	cv2.setMouseCallback("%d"%(num), mouse)
+		cv2.namedWindow("%d"%(num), cv2.WINDOW_NORMAL)
+		cv2.imshow("%d"%(num), img)
+		cv2.setMouseCallback("%d"%(num), mouse)
 
 
-		#Analises
-	print("Press 'l' to analyze the legs, 'b' to analyze the body, 'enter' to end analyzing the image, and 'esc' to end the process")
-	key = 98
-	key = cv2.waitKey(0)
-	print key
-	
-	if key == 27:  #esc
-		break
-	while ((key==98)or(key==108)):
+			#Analises
+		print("Press 'l' to analyze the legs, 'b' to analyze the body, 'enter' to end analyzing the image, and 'esc' to end the process")
+		key = 98
 		key = cv2.waitKey(0)
 		print key
 		
-	if key == 27:  #esc
-		break
+		if key == 27:  #esc
+			break
+		while ((key==98)or(key==108)):
+			key = cv2.waitKey(0)
+			print key
+			
+		if key == 27:  #esc
+			break
 
-	cv2.imwrite('GT/%s_GT.png' %(num), GT)
-	cv2.destroyAllWindows()
+		cv2.imwrite('GT/%s_GT.png' %(num), GT)
+		cv2.destroyAllWindows()
+
+main()
