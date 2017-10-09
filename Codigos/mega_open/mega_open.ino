56101b91542b1a601c5f496bfbb7f8337f47b705
abdullah-zaiter <abdullah.zaiter@hotmail.com>
2017-10-09 01:20:53 -0300

    melhorias em relacao a memoria, arduino e codigo principal

--- Codigos/mega_open/mega_open.ino
+++ Codigos/mega_open/mega_open.ino
@@ -8,10 +8,6 @@ void messageInt64Cb( const arduino_msgs::StampedInt64& r_int64_msg)
 {     
 }
 
-void messageFloat32Cb( const arduino_msgs::StampedFloat32& r_float32_msg)
-{
-}
-
 void messageFloat64Cb( const arduino_msgs::StampedFloat64& r_float64_msg)
 {
 }
--- Codigos/ros/catkin_abdullah/src/robot/src/first_test.cpp
+++ Codigos/ros/catkin_abdullah/src/robot/src/first_test.cpp
@@ -1,20 +1,36 @@
 #include "ros/ros.h"
-#include "arduino_msgs/StampedUint8.h"
-#include "arduino_msgs/StampedUint32.h"
-#include "arduino_msgs/StampedUint64.h"
-#include "arduino_msgs/StampedInt8.h"
 #include "arduino_msgs/StampedInt32.h"
 #include "arduino_msgs/StampedInt64.h"
-#include "arduino_msgs/StampedBool.h"
-#include "arduino_msgs/StampedChar.h"
 #include "arduino_msgs/StampedFloat32.h"
 #include "arduino_msgs/StampedFloat64.h"
-#include "arduino_msgs/StampedString.h"
 #include <sstream>
 #include <math.h>
 #include <vector>
 
 using namespace std;
+
+ros::NodeHandle nh;
+
+ros::Publisher pubM_int64;
+ros::Publisher pubM_float64;
+
+
+ros::Publisher pubN_int32;
+ros::Publisher pubN_float32;
+
+
+ros::Subscriber subM_int64;
+ros::Subscriber subM_float64;
+
+ros::Subscriber subN_int32;
+ros::Subscriber subN_float32;
+
+#define FLOAT_MEGA 0
+#define INT_MEGA 1
+
+#define FLOAT_UNO 2
+#define INT_UNO 3
+
 
 #define X 0
 #define Y 1
@@ -159,11 +175,6 @@ void messageMFloat64Cb( const arduino_msgs::StampedFloat64& aM_float64_msg)
 	
 }
 
-void messageMFloat32Cb( const arduino_msgs::StampedFloat32& aM_float32_msg)
-{
-	
-}
-
 void messageNInt32Cb( const arduino_msgs::StampedInt32& aN_int32_msg)
 {
 	
@@ -172,6 +183,57 @@ void messageNInt32Cb( const arduino_msgs::StampedInt32& aN_int32_msg)
 void messageNFloat32Cb( const arduino_msgs::StampedFloat32& aN_float32_msg)
 {
 
+}
+
+void initROS()
+{
+
+	pubM_int64 = nh.advertise<arduino_msgs::StampedInt64>("raspberryM_int64", 1000);
+	pubM_float64 = nh.advertise<arduino_msgs::StampedFloat64>("raspberryM_float64", 1000);
+
+	pubN_int32 = nh.advertise<arduino_msgs::StampedInt32>("raspberryN_int32", 1000);
+	pubN_float32 = nh.advertise<arduino_msgs::StampedFloat32>("raspberryN_float32", 1000);
+
+	subM_int64 = nh.subscribe("arduinoM_int64", 1000, messageMInt64Cb);
+	subM_float64 = nh.subscribe("arduinoM_float64", 1000, messageMFloat64Cb);
+	
+	subN_int32 = nh.subscribe("arduinoN_int32", 1000, messageNInt32Cb);
+	subN_float32 = nh.subscribe("arduinoN_float32", 1000, messageNFloat32Cb);
+
+}
+
+void SendFloatMega(int id, double data)
+{   
+	arduino_msgs::StampedFloat64 float64_msg;
+	float64_msg.id = id;
+	float64_msg.data = data;
+	pubM_float64.publish(float64_msg);
+}
+
+
+void SendIntMega(int id, long long int data)
+{    
+	arduino_msgs::StampedInt64 int64_msg;
+	int64_msg.id = id;
+	int64_msg.data = data;
+	pubM_int64.publish(int64_msg);
+}
+
+void SendFloatUno(int id, float data)
+{    
+	arduino_msgs::StampedFloat32 float32_msg;
+	float32_msg.id = id;
+	float32_msg.data = data;
+	pubN_float32.publish(float32_msg);
+}
+
+
+void SendIntUno(int id, long int data)
+{    
+	arduino_msgs::StampedInt32 int32_msg;
+	int32_msg.id = id;
+	int32_msg.data = data;
+	pubN_int32.publish(int32_msg);
 }
 
 float DeGrausParaRadianos(float angulo)
@@ -215,57 +277,20 @@ void TrajetoriaSuaveAteAVaca(float x1, float y1, float x2, float y2, float theta
 
 int main(int argc, char **argv)
 {
+	
 	ros::init(argc, argv, "first_test");
-	ros::NodeHandle nh;
-	/*-------------------------------------------settar as paradas do arduino mega----------------------------------------*/
-	ros::Publisher pubM_int64 = nh.advertise<arduino_msgs::StampedInt64>("raspberryM_int64", 1000);
-	ros::Publisher pubM_float64 = nh.advertise<arduino_msgs::StampedFloat64>("raspberryM_float64", 1000);
-	ros::Publisher pubM_float32 = nh.advertise<arduino_msgs::StampedFloat32>("raspberryM_float32", 1000);
-	ros::Subscriber subM_int64 = nh.subscribe("arduinoM_int64", 1000, messageMInt64Cb);
-	ros::Subscriber subM_float64 = nh.subscribe("arduinoM_float32", 1000, messageMFloat64Cb);
-	ros::Subscriber subM_float32 = nh.subscribe("arduinoM_float64", 1000, messageMFloat32Cb);
-	arduino_msgs::StampedInt64 int64M_msg;	
-	arduino_msgs::StampedFloat64 float64M_msg;
-	arduino_msgs::StampedFloat32 float32M_msg;
-	/*-------------------------------------------settar as paradas do arduino uno---------------------------------------*/
-	ros::Publisher pubN_int32 = nh.advertise<arduino_msgs::StampedInt32>("raspberryN_int32", 1000);
-	ros::Publisher pubN_float32 = nh.advertise<arduino_msgs::StampedFloat32>("raspberryN_float32", 1000);
-	ros::Subscriber subN_int32 = nh.subscribe("arduinoN_int32", 1000, messageNInt32Cb);
-	ros::Subscriber subN_float32 = nh.subscribe("arduinoN_float32", 1000, messageNFloat32Cb);
-	arduino_msgs::StampedInt32 int32N_msg;	
-	arduino_msgs::StampedFloat32 float32N_msg;
-	float32N_msg.id = VEL_REF_DIR;
-	float32N_msg.data = 0.8;
+	initROS();
 	ros::Rate loop_rate(10);	
 	int count = 0;
 	while (ros::ok())
 	{
-		/*int64M_msg.data = count;
-		float32M_msg.data = count;
-		float64M_msg.data = count;
-		int32N_msg.data = count;
-		float32N_msg.data = count;
-		pubM_int64.publish(int64M_msg);
-		pubM_float64.publish(float64M_msg);
-		pubM_float32.publish(float32M_msg);
-		pubN_int32.publish(int32N_msg);
-		pubN_float32.publish(float32N_msg);*/
 		if(count==30){
-			pubN_float32.publish(float32N_msg);
-			float32N_msg.id = VEL_REF_ESQ;
-			pubN_float32.publish(float32N_msg);
-			float32N_msg.id = VEL_REF_DIR;
-			pubN_float32.publish(float32N_msg);
+			SendFloatUno(VEL_REF_DIR,0.8);
+			SendFloatUno(VEL_REF_ESQ,0.8);
 		}else if(count == 85){
-			float32N_msg.data = 0.0;
-			pubN_float32.publish(float32N_msg);
-			float32N_msg.id = VEL_REF_ESQ;
-			pubN_float32.publish(float32N_msg);
-			float32N_msg.id = VEL_REF_DIR;
-			pubN_float32.publish(float32N_msg);
-			int32N_msg.id = GIRA;
-			int32N_msg.data = 90;
-			pubN_int32.publish(int32N_msg);
+			SendFloatUno(VEL_REF_DIR,0);
+			SendFloatUno(VEL_REF_ESQ,0);
+			SendIntUno(GIRA, 90);
 		}	
 		ros::spinOnce();
 		loop_rate.sleep();
