6e807b53bd8f29e8e2e18e4572277e2a522d26a1
GabrielPiresIduarte <gabrieliduarte@hotmail.com>
2017-11-03 01:29:39 -0200

    codigo atrasado no commit
    Bugs de identação

--- Codigos/Garra/teste_drive_tdtec_motor_passo/VisaoGarra/copo.py
+++ Codigos/Garra/teste_drive_tdtec_motor_passo/VisaoGarra/copo.py
@@ -4,11 +4,11 @@ import serial
 import time
 
 def corrigePosGarra (posicao_garra):
-    global arduino
-    arduino.write('m')
-    arduino.write(chr (posicao_garra))
+    #lobal arduino
+    #arduino.write('m')
+    #arduino.write(chr (posicao_garra))
 
-    time.sleep(0.1)
+    time.sleep(1)
     return 0
 
 def andaRobo ():
@@ -23,140 +23,126 @@ def identificaCopo (imagem):
         return 1
     return 0
 
-def copoFora (posGarra):
+def copoFora (posGarra, cap):
     pos_real = 0
     posicao_garra = posGarra
     delay = 2000
 
     while(1):
         try:
-            cap = cv2.VideoCapture(1)
+            #for i in range (0, 19):
+        	    #retr = cap.grab()
+    	        #retr, frame = cap.retrieve(retr)
+
             ret, frame = cap.read()
-            ret, frame = cap.read()
-            cap.release()
-            del cap
-            cv2.namedWindow('frame', cv2.WND_PROP_OPENGL)
-            cv2.imshow('frame', frame)
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-
-            x2Esq = posicao_garra - 30 #210
-            x1Esq = x2Esq - 55 #290
-            y1Esq = 420
-            y2Esq = 475
-
-            #imgcopoEsquerda = frame [y1Esq:y2Esq, x1Esq:x2Esq]
-            #cv2.namedWindow('imgcopoEsquerda', cv2.WND_PROP_OPENGL)
-            #cv2.imshow('imgcopoEsquerda', imgcopoEsquerda)
-
-            #esqH,esqS,esqV = cv2.split(imgcopoEsquerda)
-            #ret, binEsqH = cv2.threshold(esqH,60,1, cv2.THRESH_BINARY_INV)
-
-            #copoNaEsquerda = identificaCopo (binEsqH)
-
             x1Dir = posicao_garra + 25#365
-            x2Dir = x1Dir + 75#445
+            x2Dir = x1Dir + 80#445
             y1Dir = 420
             y2Dir = 475
             imgCopoDireita = frame [y1Dir:y2Dir, x1Dir:x2Dir]
+
+            frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+            cv2.rectangle(frame,(x1Dir,y1Dir),(x2Dir,y2Dir),(0,255,0),3)
+            cv2.namedWindow('frame', cv2.WND_PROP_OPENGL)
+            cv2.imshow('frame', frame)
+
             dirH,dirS,dirV = cv2.split(imgCopoDireita)
             ret, binDirH = cv2.threshold(dirH,60,1, cv2.THRESH_BINARY_INV)
 
             copoNaDireita= identificaCopo (binDirH)
 
-            char = cv2.waitKey(delay)
-            cv2.namedWindow('imgCopoDireita', cv2.WND_PROP_OPENGL)
-            cv2.imshow('imgCopoDireita', imgCopoDireita)
+            #char = cv2.waitKey(delay)
+            #cv2.namedWindow('imgCopoDireita', cv2.WND_PROP_OPENGL)
+            #cv2.imshow('imgCopoDireita', imgCopoDireita)
 
-            cv2.namedWindow('Hsvframe', cv2.WINDOW_OPENGL)
-            cv2.imshow('Hsvframe',dirH)
+            #cv2.namedWindow('Hsvframe', cv2.WINDOW_OPENGL)
+            #cv2.imshow('Hsvframe',dirH)
 
             cv2.namedWindow('thresh1', cv2.WINDOW_OPENGL)
             bin2 = binDirH*255
             cv2.imshow('thresh1',bin2)
 
-            cv2.namedWindow('thresh3', cv2.WINDOW_OPENGL)
-            bin3 = binEsqH*255
-            cv2.imshow('thresh3',bin3)
-            print ('a4')
             if copoNaDireita == 0:# and copoNaEsquerda == 0:
                 if posicao_garra == posGarra:
-                    print ('a1')
-                    posicao_garra += 24
-                    print ('a2')
+                    posicao_garra += 35
                     pos_real += 30
-                    print ('a3')
                     corrigePosGarra (pos_real)
+                    time.sleep(2)
                     #cv2.waitKey ()
                 else:
-                    print ('a12312')
+                    pos_real += 5
+                    corrigePosGarra (pos_real)
                     return 1
 
             elif copoNaDireita == 1:
-                print ('a6')
-                posicao_garra += 8
-                pos_real += 10
+                posicao_garra += 9
+                pos_real += 11
                 corrigePosGarra (pos_real)
 
-            #elif copoNaEsquerda == 1:
-                #print ('a7')
-                #posicao_garra -= 5
-                #pos_real -= 12
-                #corrigePosGarra (pos_real)
-            print ('a5')
-            if char == ord(','):
-                delay = delay*2
-            if char == ord('.'):
-                delay = delay/2
-            if char == ord('q'):
-                break
-            if char == ord('p'):
-                cv2.waitKey ()
-            #cv2.waitKey ()
         except Exception as e:
             print 'Erro'
             return 0
-    # When everything done, release the capture
 
 
-def copoDentro (posicao_garra, cap):
+def copoDentro (posGarra):
+    cap = cv2.VideoCapture(0)
     posicao_garra = posGarra
     while(1):
         try:
+            #for i in range (0, 19):
+        	#    retr = cap.grab()
+    	    #    retr, frame = cap.retrieve(retr)
+
             ret, frame = cap.read()
+
+    	    cv2.namedWindow('frame', cv2.WND_PROP_OPENGL)
+            cv2.imshow('frame', frame)
+
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-            x2 = posicao_garra-25 #210
-            x1 = posicao_garra+55 #290
-            y1 = 370
+            x2 = posicao_garra-30 #210
+            x1 = posicao_garra+60 #290
+            y1 = 415
             y2 = 450
-
             imgCopoDentro = frame [y1:y2, x1:x2]
+    	    print ('5')
 
-            canalH,canalS,canalV = cv2.split(imgCopoDentro)
-            ret, binH = cv2.threshold(canalH,100,1, cv2.THRESH_BINARY_INV)
+    	    cv2.namedWindow('imgCopo', cv2.WND_PROP_OPENGL)
+                cv2.imshow('imgCopo', imgCopoDentro)
+    	    print ('6')
 
-            copoDentro = identificaCopo (binH)
+                canalH,canalS,canalV = cv2.split(frame)
+    	    print ('1')
+                ret, binH = cv2.threshold(canalH,60,1, cv2.THRESH_BINARY_INV)
+    	    print ('4')
 
-            if copoDentro == 0:
-                return 1
+                copoDentro = identificaCopo (binH)
+    	    print ('2')
 
-            else:
-                andaRobo ()
+                if copoDentro == 0:
+                    return 1
+
+                else:
+    		print ("aqui")
+    		cv2.waitKey()
+                    #andaRobo ()
+    	    print ('3')
 
         except Exception as e:
             print 'Erro'
             return 0
 
 #use ttyUSB0 se for o caso, ou se for Windows, COMx.
-arduino = serial.Serial('/dev/ttyACM0',9600,timeout=2)
-a = arduino.read()
-time.sleep(3)
-cv2.waitKey()
-#cap = cv2.VideoCapture(1)
-a = copoFora (250)
+#arduino = serial.Serial('/dev/ttyACM1',9600,timeout=2)
+#a = arduino.read()
+#time.sleep(3)
+cap = cv2.VideoCapture(0)
+#cv2.waitKey()
+a = copoFora (250, cap)
 
-arduino.write('f')
-    #b = copoDentro (cap)
+#arduino.write('f')
+#b = copoDentro (296)
 #cap.release()
 cv2.destroyAllWindows()
